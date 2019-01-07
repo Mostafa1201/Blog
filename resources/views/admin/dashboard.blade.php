14c59eeed22600076ed3e94da1637e7ad86dad55
Mostafa <mostafa1201@hotmail.com>
2019-01-07 21:29:57 +0200

    adding links

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -41,15 +41,17 @@
                 </a>
             </div>
             <div class="col-lg-3 col-sm-6 col-xs-12">
-                <div class="info-box bg-light-green hover-expand-effect">
-                    <div class="icon">
-                        <i class="fa fa-user" aria-hidden="true"></i>
+                <a href="{{ url('/') }}">
+                    <div class="info-box bg-light-green hover-expand-effect">
+                        <div class="icon">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Posts</div>
+                            <div class="counter" data-count="{{ $posts }}">0</div>
+                        </div>
                     </div>
-                    <div class="content">
-                        <div class="text">Posts</div>
-                        <div class="counter" data-count="{{ $posts }}">0</div>
-                    </div>
-                </div>
+                </a>
             </div>
             <div class="col-lg-3 col-sm-6 col-xs-12">
                 <div class="info-box bg-orange hover-expand-effect">
